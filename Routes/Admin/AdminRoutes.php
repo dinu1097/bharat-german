c5157c669582e5d9df9468a64f00ad455bcd3944
dinu1097 <dinnis067@gmail.com>
2024-02-21 14:56:43 +0530

    updates

--- Routes/Admin/AdminRoutes.php
+++ Routes/Admin/AdminRoutes.php
@@ -2,7 +2,7 @@
 // Assuming you have a database connection here
 // Include the SearchFunctions class
 include '../../controllers/Admin/FlightController.php';
-include '../../controllers/Admin/HotelController.php';
+// include '../../controllers/Admin/HotelController.php';
 
 
 // $action = $_POST['action'];
@@ -13,8 +13,8 @@ include '../../controllers/Admin/HotelController.php';
 // $controller->uploadCSV($data, $tmp_file);
 
 echo "welcome";
-            $controller = new HotelController();
-            $controller->uploadCSV($data, $tmp_file);
+            // $controller = new HotelController();
+            // $controller->uploadCSV($data, $tmp_file);
 // // echo $action; 
 // // Check if the form is submitted
 // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
--- controllers/Admin/FlightController.php
+++ controllers/Admin/FlightController.php
@@ -3,7 +3,7 @@ include "../../DatabaseConnection/databaseConnection.php";
 
 class FlightController
 {
-    public function uploadCSV($data, $tmp_file)
+    public function uploadCSVFlight($data, $tmp_file)
     {
         // Check if a CSV file is uploaded
         if (isset($_FILES['csvFile']) && $_FILES['csvFile']['error'] == UPLOAD_ERR_OK) {
@@ -33,6 +33,41 @@ class FlightController
                     // Example SQL query:
                     $query = "INSERT INTO flights (airline_name, logo, flight_date, flying_from, flying_to, trip_type, flight_class, fare_price)
                     VALUES ('$airline_name', '$logo', '$flight_date', '$flying_from', '$flying_to', '$trip_type', '$flight_class', '$fare_price')";
+                    mysqli_query($conn, $query);
+                }
+                fclose($handle);
+            }
+        }
+    }
+    public function uploadCSVHotel($data, $tmp_file)
+    {
+        // Check if a CSV file is uploaded
+        if (isset($_FILES['csvFile']) && $_FILES['csvFile']['error'] == UPLOAD_ERR_OK) {
+            // Temporary file name
+            $tmp_file = $_FILES['csvFile']['tmp_name'];
+
+            // Open uploaded CSV file
+            $database = new DatabaseConnection();
+            $conn = $database->getConn();
+            if (($handle = fopen($tmp_file, "r")) !== FALSE) {
+                // Skip the first row (headers)
+                fgetcsv($handle, 1000, ",");
+
+                // Loop through each row in the CSV file
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    // Insert data into the database
+                    $city = $data[1];
+                    $checkin_date = $data[2];
+                    $checkout_date = $data[3];
+                    $room_type = $data[4];
+                    $guest_name = $data[5];
+                    $email = $data[6];
+                    $contact_number = $data[7];
+
+                    // Here you should perform database insertion
+                    // Example SQL query:
+                    $query = "INSERT INTO hotels (city, checkin_date, checkout_date, room_type, guest_name, email, contact_number)
+                    VALUES ('$city', '$checkin_date', '$checkout_date', '$room_type', '$guest_name', '$email', '$contact_number')";
                     mysqli_query($conn, $query);
                 }
                 fclose($handle);
